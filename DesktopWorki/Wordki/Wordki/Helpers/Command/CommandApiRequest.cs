b6ddf020879441a245a9a682789118eda6cd2bdf
vesper_moto <kamilsoloducha@gmail.com>
2017-08-29 09:08:46 +0200

    Changes in ICommand interface

---
+++ DesktopWorki/Wordki/Wordki/Helpers/Command/CommandApiRequest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wordki.Models.Connector;
+
+namespace Wordki.Helpers.Command
+{
+    public class CommandApiRequest : ICommand
+    {
+        public Action<ApiResponse> OnCompleteCommand { get; set; }
+        private ApiRequest Request { get; set; }
+
+        public CommandApiRequest(ApiRequest pRequest)
+        {
+            Request = pRequest;
+        }
+
+        public bool Execute()
+        {
+            Request.PrepareMessage();
+            ApiConnector lConnector = new ApiConnector();
+            ApiResponse lResponse = lConnector.SentRequest(Request);
+            if (OnCompleteCommand != null && lResponse != null)
+            {
+                OnCompleteCommand(lResponse);
+            }
+            return lResponse != null && !lResponse.IsError;
+        }
+    }
+}
---
+++ DesktopWorki/Wordki/Wordki/Helpers/Command/SimpleCommandAsync.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wordki.Helpers.Command
+{
+    public class SimpleCommandAsync : ICommand
+    {
+        private Func<Task<bool>> Action { get; set; }
+
+        public SimpleCommandAsync(Func<Task<bool>> pAction)
+        {
+            Action = pAction;
+        }
+
+        public bool Execute()
+        {
+            try
+            {
+                Action.Invoke();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
