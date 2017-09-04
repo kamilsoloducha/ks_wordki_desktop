72bce5bee8c02ba6e7d1a77ead54fbca8e0767d2
vesper_moto <kamilsoloducha@gmail.com>
2017-09-04 11:18:24 +0200

    Recreate WordkiRepository project - those are some problem with migration operations

---
+++ DesktopWorki/Wordki/WordkiRepository/DatabaseContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WordkiRepository.Model;
+
+namespace WordkiRepository
+{
+    public class DatabaseContext : DbContext
+    {
+
+        private static string DatabasePath = AppDomain.CurrentDomain.BaseDirectory + "Database\\database.sqlite";
+
+        static DatabaseContext()
+        {
+            if (File.Exists(DatabasePath))
+            {
+                return;
+            }
+            SQLiteConnection.CreateFile(DatabasePath);
+        }
+
+        public DatabaseContext() : base(new SQLiteConnection($"Data Source={DatabasePath};Version=3;"), false)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
