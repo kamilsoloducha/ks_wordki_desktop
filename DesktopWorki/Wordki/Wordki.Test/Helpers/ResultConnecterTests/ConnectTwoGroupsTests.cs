60290dd61677de8cdce470081f413d633df771b1
vesper <kamilsoloducha@gmail.com>
2017-11-22 20:14:25 +0100

    change groupmanager view

---
+++ DesktopWorki/Wordki/Wordki.Test/Helpers/ResultConnecterTests/ConnectTwoGroupsTests.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using Repository.Models;
+using Repository.Models.Enums;
+using Wordki.Helpers.ResultConnector;
+
+namespace Wordki.Test.Helpers.ResultConnecterTests
+{
+    [TestFixture]
+    public class ConnectTwoGroupsTests
+    {
+
+        IResultConnector connector;
+        Utility util = new Utility()
+        {
+            ResultCount = 10,
+            WordCount = 0,
+        };
+
+        [SetUp]
+        public void Setup()
+        {
+            connector = new ResultConnector();
+        }
+
+        [Test]
+        public void Connect_two_groups_one_without_results_test()
+        {
+            IGroup dest = util.GetGroup();
+            util.ResultCount = 0;
+            IGroup src = util.GetGroup();
+            connector.Connect(dest, src);
+            Assert.AreEqual(0, dest.Results.Count);
+        }
+
+        [Test]
+        public void Connect_two_groups_with_one_result_test()
+        {
+            util.ResultCount = 1;
+            IGroup dest = util.GetGroup();
+            IGroup src = util.GetGroup();
+            connector.Connect(dest, src);
+            Assert.AreEqual(1, dest.Results.Count);
+        }
+
+        [Test]
+        public void Connect_two_groups_with_another_type_lesson_test()
+        {
+            util.ResultCount = 0;
+            IGroup dest = util.GetGroup();
+            IGroup src = util.GetGroup();
+            dest.AddResult(util.GetResult(lessonType: LessonType.FiszkiLesson));
+            src.AddResult(util.GetResult(lessonType: LessonType.TypingLesson));
+            connector.Connect(dest, src);
+            Assert.AreEqual(0, dest.Results.Count);
+        }
+
+        [Test]
+        public void Connect_two_groups_with_swap_lesson_type_test()
+        {
+            util.ResultCount = 0;
+            IGroup dest = util.GetGroup();
+            IGroup src = util.GetGroup();
+            dest.AddResult(util.GetResult(lessonType: LessonType.FiszkiLesson));
+            dest.AddResult(util.GetResult(lessonType: LessonType.TypingLesson));
+            src.AddResult(util.GetResult(lessonType: LessonType.TypingLesson));
+            src.AddResult(util.GetResult(lessonType: LessonType.FiszkiLesson));
+            connector.Connect(dest, src);
+            Assert.AreEqual(2, dest.Results.Count);
+        }
+
+    }
+}
---
+++ DesktopWorki/Wordki/Wordki.Test/Helpers/ResultConnecterTests/ConnectTwoResultTests.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using Repository.Models;
+using Wordki.Helpers.ResultConnector;
+using Wordki.Models;
+
+namespace Wordki.Test.Helpers.ResultConnecterTests
+{
+    [TestFixture]
+    public class ConnectTwoResultTests
+    {
+
+        IResultConnector connector;
+        IResult dest;
+        IResult src;
+
+        [SetUp]
+        public void Setup()
+        {
+            connector = new ResultConnector();
+            dest = new Result
+            {
+                Accepted = 10,
+                Correct = 10,
+                Wrong = 10,
+                Invisibilities = 10,
+            };
+            src = new Result
+            {
+                Accepted = 2,
+                Correct = 2,
+                Wrong = 2,
+                Invisibilities = 2,
+            };
+        }
+
+        [Test]
+        public void Connect_two_result_check_accepted_test()
+        {
+            connector.Connect(dest, src);
+            Assert.AreEqual(12, dest.Accepted);
+        }
+        [Test]
+        public void Connect_two_result_check_correct_test()
+        {
+            connector.Connect(dest, src);
+            Assert.AreEqual(12, dest.Correct);
+        }
+        [Test]
+        public void Connect_two_result_check_wrong_test()
+        {
+            connector.Connect(dest, src);
+            Assert.AreEqual(12, dest.Wrong);
+        }
+        [Test]
+        public void Connect_two_result_check_invisibilites_test()
+        {
+            connector.Connect(dest, src);
+            Assert.AreEqual(12, dest.Invisibilities);
+        }
+    }
+}
