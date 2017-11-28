afc4bf680d33562c05e7bf16ea08eef71cd89664
vesper_moto <kamilsoloducha@gmail.com>
2017-11-28 14:12:24 +0100

    Add test for collectionindextoindexerenabledconveter

---
+++ DesktopWorki/Wordki/Wordki.Test/ConvertersTests/CollectionIndexToIndexerEnabledConverterTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wordki.ViewModels.Dialogs;
+
+namespace Wordki.Test.ConvertersTests
+{
+    [TestFixture]
+    public class CollectionIndexToIndexerEnabledConverterTest
+    {
+
+        Utility util = new Utility();
+
+        [Test]
+        public void Test()
+        {
+            CollectionIndexToIndexerEnabledConverter converter = new CollectionIndexToIndexerEnabledConverter();
+            var list = util.GetGroup().Results;
+            bool result = (bool)converter.Convert(list[0], typeof(bool), list, CultureInfo.CurrentUICulture);
+            Assert.IsTrue(result);
+        }
+
+    }
+}
