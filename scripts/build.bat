47172cdda71e7cc87f78b2704dc0cf20e43a7ebd
vesper0990 <kamilsoloducha@gmail.com>
2018-01-31 21:13:49 +0100

    changes in built.bat

--- scripts/build.bat
+++ scripts/build.bat
@@ -4,11 +4,16 @@ echo "Build started"
 REM -- set parameters
 set msBuildPath="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
 set zipPath="c:\Program Files\7-Zip\7z.exe"
+set winScpPath="e:\Downloads\WinSCP-5.11.3-Portable\winscp.com"
 set outputPath="C:\Temp\Wordki"
 set outputZipFile="Wordki.zip"
+set username=%1
+set password=%2
+set hostkey="ssh-ed25519%20256%20b2:39:f8:2c:6f:e5:bb:fb:38:02:63:84:99:1e:ff:b5"
 
 REM -- remove output path to be sure that nothing is there
 del /s /q %outputPath%
+
 
 REM -- remove output zip file
 del /s /q %outputPath%\..\wordki.zip
@@ -30,7 +35,11 @@ move *.pdb lib/
 cd ..
 
 REM -- zip whole directory to output zip file
-call %zipPath% a %outputZipFile% .\Wordki\*
+call %zipPath% a %outputZipFile% %outputPath%\*
+if not %errorlevel% == 0 goto:error
+
+REM -- Send zip file to server
+call %winScpPath% /command "open sftp://%username%:%password%@37.233.102.223 -hostkey='%hostkey%'" "put %outputZipFile% /home/oazachaosu/" "exit"
 if not %errorlevel% == 0 goto:error
 
 echo "Build successful"
